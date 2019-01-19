2b5cd976d6726a1bf45a3b685a2ad0bffe448a8b
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-01-19 15:04:36 -0700

    added file to host all CS313 assignments

---
+++ web/Home_Page/cs313_assign_page.php
@@ -0,0 +1,6 @@
+<!-- For Navbar -->
+<?php
+// By using require here, instead of include, we have decided that if that nav.php
+// page is not available, we want the page to crash.
+require "nav.php";
+?>
