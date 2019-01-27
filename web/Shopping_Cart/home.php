240677a222dab1e4fcdf0cb7e0e5e3964fbd3088
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-01-26 20:02:26 -0700

    Added sass and navbar code

--- web/Shopping_Cart/home.php
+++ web/Shopping_Cart/home.php
@@ -1,3 +1,3 @@
 <?php 
-header('Location: https://morning-citadel-97793.herokuapp.com/Shopping_Cart/browse.php');
+    header('Location: https://morning-citadel-97793.herokuapp.com/Shopping_Cart/browse.php');
 ?>
