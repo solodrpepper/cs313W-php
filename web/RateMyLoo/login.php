3f4e8615845c3b406be33ef3b8f685074a87cbf6
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-09 18:06:53 -0700

    centering login

--- web/RateMyLoo/login.php
+++ web/RateMyLoo/login.php
@@ -62,7 +62,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Rate My Loo - Login</title>
 </head>
 
-<body>
+<body style="width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            display:table;
+            display:table-cell;
+            vertical-align:middle;">
     <?php require 'nav.php';?>
     <div class="container login_form">
         <div class="row justify-content-md-center">
