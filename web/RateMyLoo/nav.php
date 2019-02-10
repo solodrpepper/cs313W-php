69ff0088e2def0fabb44510b3d5912112fb815a7
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-09 21:34:26 -0700

    hope this helps

--- web/RateMyLoo/nav.php
+++ web/RateMyLoo/nav.php
@@ -27,7 +27,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
 <?php $loggedIn = ob_get_clean(); ?>
 
 <!-- Navbar elements -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light nav-pills">
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03"
       aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -49,9 +49,9 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
                echo "$notLoggedIn";
             } else {
                echo '<li class="nav-item">';
-               echo '<span></span>Welcome, ';
+               echo '<span>Welcome, ';
                echo "$uname";
-               echo ' </li>';
+               echo ' </span></li>';
                echo "$loggedIn";
             }
          ?>
