889c105ccc10693137fc17047ecd486e3d47d42a
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-09 17:07:15 -0700

    Added bootstrap links

--- web/RateMyLoo/login.php
+++ web/RateMyLoo/login.php
@@ -38,17 +38,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
     <title>Rate My Loo - Login</title>
 </head>
+
 <body>
-<?php require 'nav.php';?>
+    <?php require 'nav.php';?>
     <FORM NAME="loginForm" METHOD="POST" ACTION="login.php">
         <!-- Here the user will enter in their credentials -->
-        Email:    <INPUT TYPE='TEXT' Name='username' value="<?php print $uname;?>"
+        Email: <INPUT TYPE='TEXT' Name='username' value="<?php print $uname;?>"
             maxlength="30" required autofocus>
         Password: <INPUT TYPE='TEXT' Name='password' value="<?php print $pword;?>"
             maxlength="16" required>
@@ -60,4 +75,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <?php print $errorMessage;?>
     </p>
 </body>
+
 </html>
--- web/RateMyLoo/signup.php
+++ web/RateMyLoo/signup.php
@@ -34,21 +34,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
     <title>Rate My Loo - Sign Up</title>
 </head>
+
 <body>
-<?php require 'nav.php';?>
+    <?php require 'nav.php';?>
     <FORM NAME="signUpForm" METHOD="POST" ACTION="signup.php">
-        Email:    <INPUT TYPE='TEXT' Name='email' value="<?php print $uname;?>"><br/>
+        Email: <INPUT TYPE='TEXT' Name='email' value="<?php print $uname;?>"><br />
         Password: <INPUT TYPE='TEXT' Name='password' value="<?php print $pword;?>">
-    <p>
-        <INPUT TYPE="Submit" Name="signUpSubmit" VALUE="Register">
-    </p>
+        <p>
+            <INPUT TYPE="Submit" Name="signUpSubmit" VALUE="Register">
+        </p>
     </FORM>
-        <?php print $errorMessage;?>
+    <?php print $errorMessage;?>
 </body>
+
 </html>
