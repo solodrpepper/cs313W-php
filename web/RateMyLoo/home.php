5522b8f07d0098b3c9b56611a13bbaf818baa082
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-13 18:00:24 -0700

    formatting

--- web/RateMyLoo/home.php
+++ web/RateMyLoo/home.php
@@ -73,7 +73,7 @@ $statement->execute();
 $item_count = 0;
 
 // start container div
-echo "<div>\n";
+echo "<div class='container'>\n";
 
 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
     // The variable "row" now holds the complete record for that
