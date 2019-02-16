14dfa1e6da7f8b65fa466fb9e545dffdf5b040b9
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-16 00:46:08 -0700

    asdfg

--- web/RateMyLoo/signup.php
+++ web/RateMyLoo/signup.php
@@ -69,7 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // DEBUGGING
         //////////////////////
 
-        echo "<script>console.log('$response')</script>";
+        var_dump($response);
 
         ///////////////////////
         // DEBUGGING
