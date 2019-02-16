9f4121f1b9fb97b11b50d775037e8c872e637662
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-15 23:27:44 -0700

    Please

--- web/RateMyLoo/signup.php
+++ web/RateMyLoo/signup.php
@@ -77,12 +77,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     // Let's hash the password so we can send the hash to the database
                     $phash = password_hash($pword, PASSWORD_DEFAULT);
                     $statement = $db->prepare("INSERT INTO users (email, username, hash_ed, isEmailConfirmed, token) 
-                                                VALUES (:email, :username, :hash_ed, :is_male, :isEmailConfirmed, :token)");
+                                                VALUES (:email, :username, :hash_ed, :is_male, 0, :token)");
                     $statement->bindParam(':email', $email);
                     $statement->bindParam(':username', $uname);
                     $statement->bindParam(':hash_ed', $phash);
                     $statement->bindParam(':is_male', $is_male);
-                    $statement->bindParam(':isEmailConfirmed', 0);
+                    //$statement->bindParam(':isEmailConfirmed', 0);
                     $statement->bindParam(':token', $token);
                     $statement->execute();
     
