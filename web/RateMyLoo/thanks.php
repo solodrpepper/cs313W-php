492719c83602d20dafb1c4d1a6877796cee5c9ba
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-16 19:57:23 -0700

    Edited the tank you page

--- web/RateMyLoo/thanks.php
+++ web/RateMyLoo/thanks.php
@@ -1,9 +1,15 @@
 <?php
 
 //Set Refresh header using PHP.
-header( "refresh:5;url=http://thisinterestsme.com/php-forcing-https-over-http/" );
+header( "refresh:3;url=https://morning-citadel-97793.herokuapp.com/RateMyLoo/login.php" );
  
 //Print out some content for example purposes.
-echo "<h1 style='align-text: center;'>Thank you for signing up!<br/>We\'ll redirect you in just a moment to the Login screen! <br/> Happy Flushing!!<h1>";
+echo "<h2 style='align-self: center; align-text: center;'>
+        Thank you for signing up!<br/>
+        We'll redirect you in just a moment to the Login screen!
+        <br/>Happy Flushing!!<h2><br /><br />";
+
+echo "<h3 style='align-self: center; align-text: center;'>If you're not redirected in a few seconds, click here: ";
+echo "<a href='https://morning-citadel-97793.herokuapp.com/RateMyLoo/login.php'>Login</a></h3>";
 
 ?>
