b52e044b84e824e290b00a0115b19f8838cc6038
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-02-23 21:44:38 -0700

    Creating the comment section

--- web/RateMyLoo/bathroom_detail.php
+++ web/RateMyLoo/bathroom_detail.php
@@ -63,8 +63,9 @@ $statement->execute();
     <?php require 'nav.php';?>
 
     <div class='container'>
-        <!-- Picture here -->
-        <?php
+        <div class='card'>
+            <!-- Picture here -->
+            <?php
             
 
             echo $floor_value;
@@ -77,11 +78,65 @@ $statement->execute();
                 echo $comment;
                 echo "<br />-- ";
                 echo $uname;
-
             }
         ?>
-
+        </div>
     </div>
+
+    <!-- Comment section of the page -->
+    <ul class="comment-section">
+        <li class="comment user-comment">
+            <div class="info">
+                <a href="#">Anie Silverston</a>
+                <span>4 hours ago</span>
+            </div>
+            <a class="avatar" href="#">
+                <img src="images/avatar_user_1.jpg" width="35" alt="Profile Avatar" title="Anie Silverston" />
+            </a>
+            <p>Suspendisse gravida sem?</p>
+        </li>
+        <li class="comment author-comment">
+            <div class="info">
+                <a href="#">Jack Smith</a>
+                <span>3 hours ago</span>
+            </div>
+            <a class="avatar" href="#">
+                <img src="images/avatar_author.jpg" width="35" alt="Profile Avatar" title="Jack Smith" />
+            </a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse gravida sem sit amet molestie
+                portitor.</p>
+        </li>
+        <li class="comment user-comment">
+            <div class="info">
+                <a href="#">Bradley Jones</a>
+                <span>1 hour ago</span>
+            </div>
+            <a class="avatar" href="#">
+                <img src="images/avatar_user_2.jpg" width="35" alt="Profile Avatar" title="Bradley Jones" />
+            </a>
+            <p>Suspendisse gravida sem sit amet molestie portitor?</p>
+        </li>
+        <li class="comment author-comment">
+            <div class="info">
+                <a href="#">Jack Smith</a>
+                <span>1 hour ago</span>
+            </div>
+            <a class="avatar" href="#">
+                <img src="images/avatar_author.jpg" width="35" alt="Profile Avatar" title="Jack Smith" />
+            </a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisee gravida sem sit amet molestie
+                porttitor.</p>
+        </li>
+        <li class="write-new">
+            <form action="#" method="post">
+                <textarea placeholder="Write your comment here" name="comment"></textarea>
+                <div>
+                    <img src="images/avatar_user_2.jpg" width="35" alt="Profile of Bradley Jones" title="Bradley Jones" />
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </li>
+    </ul>
 
 </body>
 
